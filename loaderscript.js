1b69013d297af845579d0479d27cb22916afda66
dogbonestudios <160281713+dogbonestudios@users.noreply.github.com>
2026-07-18 13:53:55 -0400

    Update loaderscript.js

--- loaderscript.js
+++ loaderscript.js
@@ -1,3 +1,3 @@
 import sheet from './loader.css' with { type: 'css'};
 
-if sheet = setTimeout(Timmer, 50); then window.location.href = 'main.html'; = true
+if sheet = setTimeout(Timmer, 50); do window.location.href = 'main.html'; = true
